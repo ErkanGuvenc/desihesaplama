ef0e064b68c698bee28da8e3e3153ed43f69690a
Erkan Guvenc <74397338+ErkanGuvenc@users.noreply.github.com>
2020-12-24 00:00:59 +0300

    ikinci yükleme

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -19,80 +20,134 @@ class desihes extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Form(
-          key: _degerKey,
-          child: Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextFormField(
-                  decoration: InputDecoration(
-                      labelText: "ENİ",
-                      labelStyle: TextStyle(color: Colors.red),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
+        body: Container(
+          color: Colors.amber[300],
+          child: Form(
+            key: _degerKey,
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset('images/box.png'),
+                  Text(
+                    "KARGO DESİ HESAPLAMA",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 20),
+                  TextFormField(
+                    decoration: InputDecoration(
+                        labelText: "ENİ",
+                        labelStyle: TextStyle(color: Colors.black),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                        ),
+                        prefixIcon: Icon(
+                          FontAwesomeIcons.rulerHorizontal,
+                          color: Colors.black,
+                        )),
+                    keyboardType: TextInputType.number,
+                    onSaved: (parse) {
+                      en = int.parse(parse);
+                    },
+                  ),
+                  SizedBox(height: 15),
+                  TextFormField(
+                    decoration: InputDecoration(
+                        labelText: "BOYU",
+                        labelStyle: TextStyle(color: Colors.black),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                        ),
+                        prefixIcon: Icon(
+                          FontAwesomeIcons.rulerVertical,
+                          color: Colors.black,
+                        )),
+                    keyboardType: TextInputType.number,
+                    onSaved: (parse) {
+                      boy = int.parse(parse);
+                    },
+                  ),
+                  SizedBox(height: 15),
+                  TextFormField(
+                    decoration: InputDecoration(
+                        labelText: "AĞIRLIĞI",
+                        labelStyle: TextStyle(color: Colors.black),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                        ),
+                        prefixIcon: Icon(
+                          FontAwesomeIcons.weight,
+                          color: Colors.black,
+                        )),
+                    keyboardType: TextInputType.number,
+                    onSaved: (parse) {
+                      agirlik = int.parse(parse);
+                    },
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      RaisedButton(
+                        color: Colors.amber[500],
+                        child: Text("TEMİZLE"),
+                        onPressed: () {
+                          Navigator.pushNamed(context, '/');
+                        },
                       ),
-                      prefixIcon: Icon(Icons.comment)),
-                  keyboardType: TextInputType.number,
-                  onSaved: (parse) {
-                    en = int.parse(parse);
-                  },
-                ),
-                SizedBox(height: 15),
-                TextFormField(
-                  decoration: InputDecoration(
-                      labelText: "BOYU",
-                      labelStyle: TextStyle(color: Colors.red),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                      RaisedButton(
+                        color: Colors.amber[500],
+                        child: Text("HESAPLA"),
+                        onPressed: () {
+                          _degerKey.currentState.save();
+                          debugPrint("EN: $en, BOY: $boy, AĞIRLIK : $agirlik");
+                          desi = en * boy * agirlik;
+                          if (en != 0 && boy != 0 && agirlik != 0) {
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  title: Text("Desi Hesaplama Sonucu : $desi"),
+                                  actions: [
+                                    MaterialButton(
+                                      child: Text("Tekrar Hesapla"),
+                                      onPressed: () {
+                                        Navigator.pushNamed(context, '/');
+                                      },
+                                    ),
+                                  ],
+                                );
+                              },
+                            );
+                          } else {
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  title: Text("Yanlış Giriş Yaptınız."),
+                                  actions: [
+                                    MaterialButton(
+                                      child: Text("Tekrar Hesapla"),
+                                      onPressed: () {
+                                        Navigator.pushNamed(context, '/');
+                                      },
+                                    ),
+                                  ],
+                                );
+                              },
+                            );
+                          }
+                        },
                       ),
-                      prefixIcon: Icon(Icons.comment)),
-                  keyboardType: TextInputType.number,
-                  onSaved: (parse) {
-                    boy = int.parse(parse);
-                  },
-                ),
-                SizedBox(height: 15),
-                TextFormField(
-                  decoration: InputDecoration(
-                      labelText: "AĞIRLIĞI",
-                      labelStyle: TextStyle(color: Colors.red),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
-                      ),
-                      prefixIcon: Icon(Icons.comment)),
-                  keyboardType: TextInputType.number,
-                  onSaved: (parse) {
-                    agirlik = int.parse(parse);
-                  },
-                ),
-                SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    RaisedButton(
-                      child: Text("TEMİZLE"),
-                      onPressed: () {
-                        Navigator.pushNamed(context, '/');
-                      },
-                    ),
-                    RaisedButton(
-                      child: Text("HESAPLA"),
-                      onPressed: () {
-                        _degerKey.currentState.save();
-                        debugPrint("EN: $en, BOY: $boy, AĞIRLIK : $agirlik");
-                        desi = en * boy * agirlik;
-                        if (en != 0 && boy != 0 && agirlik != 0) {
-                          debugPrint("$desi");
-                        } else {
-                          debugPrint("hesaplama yanlış");
-                        }
-                      },
-                    ),
-                  ],
-                ),
-              ],
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
